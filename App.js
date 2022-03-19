26e7806a5f662766e7847061770425d84a502f8a
amymaryflores <amyflores125@gmail.com>
2022-03-19 12:10:34 -0700

    Update App.js
    added foundation

---
+++ App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Weather from "./Weather.js";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="London" />
+        <footer>
+          <a
+            href="https://github.com/amymaryflores/Weather-react-app"
+            alt=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open sourced code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://agitated-yalow-146329.netlify.app"
+            alt=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amy Flores
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
